edaee0ae82c11fd9b46d05532eb420e9cf709e3d
Maycon Souza <maycon.souza31@gmail.com>
2019-03-08 12:51:05 -0300

    08/03 - Classe para tirar ScreenShot

--- Mark7/src/test/java/managers/DAO.java
+++ Mark7/src/test/java/managers/DAO.java
@@ -1,32 +1,68 @@
 package managers;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.MongoCredential;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
 
 public class DAO {
-	
-	// Creating a Mongo client 
-//	MongoClientURI uri = new MongoClientURI("mongodb://aluno:qaninja@ds225078.mlab.com:25078/heroku_4m3km28x");
-//	MongoClient mongo = new MongoClient(uri);
-	
-	MongoClient mongo = new MongoClient( "ds225078.mlab.com" , 25078); 
-	
-    // Creating Credentials 
-    MongoCredential credential = MongoCredential.createCredential("aluno", "heroku_4m3km28x", "qaninja".toCharArray()); 
-	
-	// Retrieving a collection
-	MongoCollection<Document> collection = mongo.getDatabase("heroku_4m3km28x").getCollection("users");
-	
+
+//	private DBCollection collection;
+//	private Mongo mongo;
+//	private DB db;
+//	private BasicDBObject document;
+
+	private MongoClient mongo;
+	private MongoDatabase db;
+	private MongoCollection<Document> collection;
+
+	public boolean conectar() {
+		try {
+//			// Creating a Mongo client
+//			mongo = new Mongo("ds225078.mlab.com", 25078);
+//
+//			// Retrieving a collection
+//			db = mongo.getDB("heroku_4m3km28x");
+//			collection = db.getCollection("users");
+
+			mongo = new MongoClient("ds225078.mlab.com", 25078);
+			db = mongo.getDatabase("heroku_4m3km28x");
+			collection = db.getCollection("users");
+
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
 	public DAO removerUsuario(String email) {
-		collection.deleteOne(Filters.eq("email", email));
-		System.out.println("Apagou "+email);
+//		document = new BasicDBObject();
+//		document.remove("email", email);
+////		collection.remove(document);
+//		System.out.println("Apagou " + document);
+		
+		Bson filter = new Document("email", email);
+		collection.deleteOne(filter);
+		System.out.println("Apagou " + filter);
+				
 		return this;
 	}
-	
+
+	public boolean desconectar() {
+		try {
+			mongo.close();
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
 }
--- Mark7/src/test/java/steps/ScriptSteps.java
+++ Mark7/src/test/java/steps/ScriptSteps.java
@@ -2,7 +2,9 @@ package steps;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 
 import managers.DAO;
@@ -12,10 +14,15 @@ import pages.InsertTaskPage;
 import pages.LoginPage;
 import pages.PerfilPage;
 import pages.TasksPage;
+import suporte.Generator;
+import suporte.Screenshot;
 
 //Ordenando por ordem alfabética a execução
 //@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ScriptSteps {
+	
+    @Rule
+    public TestName test = new TestName();
 
 	WebDriver driver;
 	DriverManager dm = new DriverManager();
@@ -35,28 +42,34 @@ public class ScriptSteps {
 		novaTarefa = new InsertTaskPage(driver);
 		novoPerfil = new PerfilPage(driver);
 		dao = new DAO();
-
+		//Removendo usuario via API
+		driver.get("http://mark7.herokuapp.com/api/reset/batistinha@gmail.com?clean=full");
+		
 		driver.get("http://mark7.herokuapp.com/login");
 	}
 
 	@Test
-	public void realizarCadastroUsuario() {
-
-		//Removendo usuario via API
-//		driver.get("http://mark7.herokuapp.com/api/reset/batistinha@gmail.com?clean=full");
+	public void realizarCadastroUsuario() throws InterruptedException {
 		
 		//Removendo usuario via MongoDB
-		dao.removerUsuario("batistinha@gmail.com");
+//		dao.conectar();
+//		dao.removerUsuario("bruna@gmail.com");
+//		dao.desconectar();
 		
 		login.clicarCadastro();
 		cadastro.realizarCadastroUsuario("batistinha@gmail.com", "Batistinha", "bat2019");
+		Thread.sleep(2000);
 		Assert.assertEquals(task.validaUsuarioLogado(), "batistinha@gmail.com");
+		
+        //Salvando Screenshot
+        String screenshotArquivo = "C:\\Users\\msouza7\\OneDrive\\Estudostest-report\\taskit\\" + Generator.dataHoraParaArquivo()+"_"+test.getMethodName()+".png";
+        Screenshot.tirar(driver,screenshotArquivo );
 	}
-//
+
 //	@Test
 //	public void efetuarLogin() throws InterruptedException {
 //
-//		login.realizarLogin("teste123@gmail.com", "teste123");
+//		login.realizarLogin("batistinha@gmail.com", "bat2019");
 //		
 //		Thread.sleep(2000);
 //
